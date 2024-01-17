6fcb3be4af4a0391f929ef6301f05e1b77f382de
Collin <csampso@icloud.com>
2024-01-17 14:57:37 -0600

    11/17/2024

--- Unit5/src/BigFactorial.java
+++ Unit5/src/BigFactorial.java
@@ -1,7 +1,16 @@
+import java.math.BigInteger;
+
 public class BigFactorial {
     public static void main(String[] args) {
+        BigInteger x = new BigInteger("1") , y = BigInteger.valueOf(3);
+
+
+
+
+
+
         for (int i = 0; i <= 30; i++) {
-            System.out.println(i + "\t" + factorial(i));
+            System.out.println(i + "\t" + factorial(i) + "\t" + bigFactorial(i));
         }
     }
 
@@ -11,6 +20,17 @@ public class BigFactorial {
             f = f * k;
         }
         return f;
+    }
+    public static BigInteger bigFactorial(int n){
+        BigInteger f = new BigInteger("1");
+        BigInteger k = BigInteger.valueOf(n);
+        for (int i = n; i > 0; i--) {
+            f = f.multiply(k);
+            n--;
+            k = BigInteger.valueOf(n);
+        }
+        return f;
+
     }
 
 }
