75c3d5974e3cdb9c552a9f3cf93b528843b6183a
CollinPC <csampso@icloud.com>
2024-01-18 17:21:28 -0600

    11/18/24

--- Unit5/src/WorkingWithObjects.java
+++ Unit5/src/WorkingWithObjects.java
@@ -17,9 +17,9 @@ public class WorkingWithObjects {
 
         System.out.println(p1);
 
-        p2.setLocation(3.14,3.14);
+        p2.setLocation(3.499999999,3.51);
         
-        System.out.println(p2);
+        System.out.println("THIS" + p2);
 
         p1.setLocation(9,10);
 
@@ -31,7 +31,17 @@ public class WorkingWithObjects {
 
         System.out.println(p1);
 
+
+
     }
 }
+
+// 1. Who wrote the Point source code?
+// Sami Shaio
+// 2. Roughly how much of the file is comments vs. actual code?
+// 95% comments with occasional code sprinkled in
+// 3. How does rounding of a double work in the setLocation method?
+// It rounds exactly like we would expect it to round where it rouns up from .5 on and down
+// from less than .5
 
 
