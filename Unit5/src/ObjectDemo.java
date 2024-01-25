0fc2a51b933902993004950e2a64690e5a08d4ed
Collin <csampso@icloud.com>
2024-01-25 14:53:21 -0600

    1/25/24

--- Unit5/src/ObjectDemo.java
+++ Unit5/src/ObjectDemo.java
@@ -2,11 +2,13 @@ public class ObjectDemo {
     public static void main(String[] args){
         Dot d1 = new Dot();
         Dot d2 = new Dot(1,2,3);
+        Time t1 = new Time();
         //d1.x = -1;  //error because x is (correctly) private 
         d1.z = 5;
         d2.move(1,0,-1);
         System.out.println(d1);
         System.out.println(d2);
         System.out.println(d1.distance(d2));
+        t1.setHour(600);
     }
 }
--- Unit5/src/Time.java
+++ Unit5/src/Time.java
@@ -1,42 +1,83 @@
 public class Time {
-    private int hour;
-    private int minute;
-    private double second;
+    
+    private double totalSeconds;
+    private final double SECONDS_IN_DAY = 24 * 60 * 60;
+   
     
     public Time() {
-        this.hour = 0;
-        this.minute = 0;
-        this.second = 0.0;
+        this.totalSeconds = 0;
+    }
+
+    public Time(double totalSeconds) {
+        this.totalSeconds = totalSeconds;
     }
     
     public Time(int hour, int minute, double second) {
-        this.hour = hour;
-        this.minute = minute;
-        this.second = second;
+        this.totalSeconds += hour * 60 * 60;
+        this.totalSeconds += minute * 60;
+        this.totalSeconds += second;
     }
 
     public void setHour(int hour) {
+        if(hour >= 0 && hour <= 12) {
+            //TODO update total number of seconds to include new hour with old min and sec
+            //getMin and getSec will be useful
         this.hour = hour;
+
+        } else {
+            throw new IllegalArgumentException("Idoit: ");
+        }
     }
 
     public void setMinute(int minute) {
+        if(minute >= 0 && minute <= 60) {
+
         this.minute = minute;
+        
+        } else {
+            throw new IllegalArgumentException("Idoit: ");
+        }
     }
 
     public void setSecond(double second) {
-        this.second = second;
+        if(second >= 0 && second <= 60) {
+
+            this.second = second;
+            
+            } else {
+                throw new IllegalArgumentException("Idoit: ");
+            }
     }
     
     public int getHour() {
-        return this.hour;
+        //TODO whole num hour
+        return -1;
     }
 
     public int getMinute() {
-        return this.minute;
+        //TODO whole num min
+        return -1;
     }
 
     public double getSecond() {
-        return this.second;
+        //TODO whole num sec
+        return -1;
     }
+    public String toString() {
+        return String.format("%02d:%02d:%04.1f\n",
+        this.hour, this.minute, this.second);
+        }
+        
 
+    public boolean equals(Time that){
+        final double DELTA = .001;
+        return Math.abs(this.totalSeconds - that.totalSeconds) < DELTA;
+    }
+    public Time add(Time other){
+        double sum = (this.totalSeconds + other.totalSeconds) % SECONDS_IN_DAY;
+        return new Time(sum);
+    }
+    public Time subtract(Time other){
+        return null;
+    }
 }
