b9257a56581794f99eba5e94556c16ecd396ef4f
CollinPC <csampso@icloud.com>
2024-01-25 18:16:15 -0600

    11/25/24 complete

--- Unit5/src/ObjectDemo.java
+++ Unit5/src/ObjectDemo.java
@@ -2,13 +2,24 @@ public class ObjectDemo {
     public static void main(String[] args){
         Dot d1 = new Dot();
         Dot d2 = new Dot(1,2,3);
-        Time t1 = new Time();
+        Time t1 = new Time(13345.2);
+        Time t2 = new Time(13345.2);
         //d1.x = -1;  //error because x is (correctly) private 
         d1.z = 5;
         d2.move(1,0,-1);
         System.out.println(d1);
         System.out.println(d2);
         System.out.println(d1.distance(d2));
-        t1.setHour(600);
+        
+        System.out.println(t1.getHour());
+        System.out.println(t1.getMinute());
+        System.out.println(t1.getSecond());
+        System.out.println(t1);
+        t1.setHour(7);
+        System.out.println(t1);
+        System.out.println(t1.subtract(t2));
+        t1 = new Time(13345.2);
+        t2 = new Time(13345.25);
+        System.out.println(t1.equals(t2));
     }
 }
--- Unit5/src/Time.java
+++ Unit5/src/Time.java
@@ -1,9 +1,8 @@
 public class Time {
-    
+
     private double totalSeconds;
     private final double SECONDS_IN_DAY = 24 * 60 * 60;
-   
-    
+
     public Time() {
         this.totalSeconds = 0;
     }
@@ -11,7 +10,7 @@ public class Time {
     public Time(double totalSeconds) {
         this.totalSeconds = totalSeconds;
     }
-    
+
     public Time(int hour, int minute, double second) {
         this.totalSeconds += hour * 60 * 60;
         this.totalSeconds += minute * 60;
@@ -19,10 +18,10 @@ public class Time {
     }
 
     public void setHour(int hour) {
-        if(hour >= 0 && hour <= 12) {
-            //TODO update total number of seconds to include new hour with old min and sec
-            //getMin and getSec will be useful
-        this.hour = hour;
+        double tempSum = getSecond() + getMinute() * 60;
+        if (hour >= 0 && hour <= 12) {
+
+            this.totalSeconds = hour * 60 * 60 + tempSum;
 
         } else {
             throw new IllegalArgumentException("Idoit: ");
@@ -30,54 +29,58 @@ public class Time {
     }
 
     public void setMinute(int minute) {
-        if(minute >= 0 && minute <= 60) {
+        double tempSum = getSecond() + getHour() * 60 * 60;
+        if (minute >= 0 && minute <= 60) {
+            this.totalSeconds = minute * 60 + tempSum;
 
-        this.minute = minute;
-        
         } else {
             throw new IllegalArgumentException("Idoit: ");
         }
     }
 
     public void setSecond(double second) {
-        if(second >= 0 && second <= 60) {
+        double tempSum = getMinute() * 60 + getHour() * 60 * 60;
+        if (second >= 0 && second <= 60) {
 
-            this.second = second;
-            
-            } else {
-                throw new IllegalArgumentException("Idoit: ");
-            }
+            this.totalSeconds = second + tempSum;
+
+        } else {
+            throw new IllegalArgumentException("Idoit: ");
+        }
     }
-    
+
     public int getHour() {
-        //TODO whole num hour
-        return -1;
+
+        return (int) (totalSeconds / (60 * 60));
     }
 
     public int getMinute() {
-        //TODO whole num min
-        return -1;
+
+        return (int) (totalSeconds % (60 * 60)) / 60;
     }
 
     public double getSecond() {
-        //TODO whole num sec
-        return -1;
+
+        return (int) (totalSeconds % (60 * 60)) % 60;
     }
+
     public String toString() {
         return String.format("%02d:%02d:%04.1f\n",
-        this.hour, this.minute, this.second);
-        }
-        
+                getHour(), getMinute(), getSecond());
+    }
 
-    public boolean equals(Time that){
+    public boolean equals(Time that) {
         final double DELTA = .001;
         return Math.abs(this.totalSeconds - that.totalSeconds) < DELTA;
     }
-    public Time add(Time other){
+
+    public Time add(Time other) {
         double sum = (this.totalSeconds + other.totalSeconds) % SECONDS_IN_DAY;
         return new Time(sum);
     }
-    public Time subtract(Time other){
-        return null;
+    
+    public Time subtract(Time other) {
+        double difference = this.totalSeconds - other.totalSeconds;
+        return new Time(difference);
     }
 }
