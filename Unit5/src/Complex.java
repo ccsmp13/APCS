6bf003490b57ae69cd60d41e82fe907f6a8bb9b6
Collin <csampso@icloud.com>
2024-01-31 12:58:56 -0600

    1/31/24

---
+++ Unit5/src/Complex.java
@@ -0,0 +1,16 @@
+public class Complex {
+    private double real;
+    private double imag;
+
+
+    public Complex(double realPart, double imaginaryPart){
+        this.real = realPart;
+        this.imag = imaginaryPart;
+    }
+    public Complex(){
+        this.real = 0;
+        this.imag = 0;
+        
+    }
+    
+}
